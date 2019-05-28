8a2099023328c07e90bb888d2a87a317d53269d4
davidbridges123 <david.bridges@live.com>
2019-05-28 13:02:10 -0600

    pseudocode js

---
+++ assests/hangman.js
@@ -0,0 +1,18 @@
+
+// need to make array for the cartoon names
+
+var cartoons = ["Tom"]
+
+// need to make variables for values
+
+// counter variables
+
+// Game function
+
+// reset function
+
+//check letters function
+
+// won check funtion 
+
+// 
